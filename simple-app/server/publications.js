b58541efc572c3550651ae60dd4379ec1123e5ad
stormalex <lvjingbang@163.com>
2016-08-27 22:13:37 +0800

    router use

--- simple-app/server/publications.js
+++ simple-app/server/publications.js
@@ -1,3 +1,3 @@
-Meteor.publish('posts', function(author) {
-	return Posts.find({author:author}, {fields:{url:false}});		//只发布某一个作者的，但是隐藏url字段
+Meteor.publish('posts', function() {
+	return Posts.find();		//只发布某一个作者的，但是隐藏url字段
 })
